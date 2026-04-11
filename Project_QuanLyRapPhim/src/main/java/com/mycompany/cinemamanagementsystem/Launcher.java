cd63cd5b88b44c9289f29307406bd099d78083a7
phandinhtinh <107018227+phandinhtinh@users.noreply.github.com>
2026-04-11 23:53:33 +0700

    Delete Project_QuanLyRapPhim directory

--- Project_QuanLyRapPhim/src/main/java/com/mycompany/cinemamanagementsystem/Launcher.java
+++
@@ -1,8 +0,0 @@
-package com.mycompany.cinemamanagementsystem;
-
-public class Launcher {
-    public static void main(String[] args) {
-        // Dùng class này để mồi chạy class App, giúp né lỗi của JavaFX
-        App.main(args); 
-    }
-}
