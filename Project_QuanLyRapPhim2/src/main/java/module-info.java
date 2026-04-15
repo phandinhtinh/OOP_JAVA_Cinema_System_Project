ee202317625cb65467c35a331f5e56d8000f50e3
phandinhtinh <107018227+phandinhtinh@users.noreply.github.com>
2026-04-15 10:08:15 +0700

    Delete Project_QuanLyRapPhim2 directory

--- Project_QuanLyRapPhim2/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.mycompany.cinemamanagementsystem {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.sql;
-
-    opens com.mycompany.cinemamanagementsystem to javafx.fxml;
-    exports com.mycompany.cinemamanagementsystem;
-}
